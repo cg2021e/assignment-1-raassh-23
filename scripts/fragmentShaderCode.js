3ab7ecabd1035876f617f9478f143d91c71f9218
raassh-23 <muh.nur.abdurrauf@gmail.com>
2021-12-04 03:58:53 +0700

    change shader

--- scripts/fragmentShaderCode.js
+++ scripts/fragmentShaderCode.js
@@ -1,8 +1,49 @@
 export const fragmentShaderCode = `
     precision mediump float;
+    precision mediump float;
     varying vec3 vColor;
 
+    uniform vec3 uLightConstant;
+    uniform float uAmbientIntensity;
+
+    varying vec3 vNormal;
+    uniform mat3 uNormalModel;
+    
+    uniform vec3 uLightPosition;
+    varying vec3 vPosition;
+
+    uniform vec3 uViewerPosition;
+
+    uniform float uShininessConstant;
+
     void main() {
-        gl_FragColor = vec4(vColor, 1.0);
+        vec3 ambient = uLightConstant * uAmbientIntensity;
+
+        vec3 lightDirection = uLightPosition - vPosition;
+
+        vec3 normalizedLight = normalize(lightDirection);
+        vec3 normalizedNormal = normalize(uNormalModel * vNormal);
+        float cosTheta = dot(normalizedLight, normalizedNormal);
+
+        vec3 diffuse = vec3(0.0, 0.0, 0.0);
+        if (cosTheta > 0.) {
+            float diffuseIntensity = cosTheta;
+            diffuse = uLightConstant * diffuseIntensity;
+        }
+
+        vec3 reflector = reflect(-lightDirection, normalizedNormal);
+        vec3 normalizedReflector = normalize(reflector);
+        vec3 normalizedViewer = normalize(uViewerPosition - vPosition);
+        float cosPhi = dot(normalizedReflector, normalizedViewer);
+
+        vec3 specular = vec3(0.0, 0.0, 0.0);
+        if (cosPhi > 0.0) {
+            float specularIntensity = pow(cosPhi, uShininessConstant);
+            specular = uLightConstant * specularIntensity;
+        }
+
+        vec3 phong = ambient + diffuse + specular;
+
+        gl_FragColor = vec4(phong * vColor, 1.0);
     }
 `
--- scripts/vertexShaderCode.js
+++ scripts/vertexShaderCode.js
@@ -1,12 +1,22 @@
 export const vertexShaderCode = `
-    attribute vec2 aPosition;
+    attribute vec3 aPosition;
     attribute vec3 aColor;
     varying vec3 vColor;
-    uniform mat4 uTranslationMatrix;
+
+    uniform mat4 uModel;
+    uniform mat4 uView;
+    uniform mat4 uProjection;
+
+    attribute vec3 aNormal;
+    varying vec3 vNormal;
+
+    varying vec3 vPosition;
 
     void main() {
-        gl_Position = uTranslationMatrix * vec4(aPosition, 0.0, 1.0);
-        gl_PointSize = 10.0;
+        gl_Position = uProjection * uView * uModel * (vec4(aPosition * 2.0 / 3.0, 1.0));
+
         vColor = aColor;
+        vNormal = aNormal;
+        vPosition = (uModel * (vec4(aPosition * 2.0 / 3.0, 1.0))).xyz;
     }
 `
