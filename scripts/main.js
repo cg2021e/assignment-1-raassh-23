ee3154974e15db07ebd373155aa268f5391cfc89
raassh-23 <muh.nur.abdurrauf@gmail.com>
2021-12-10 10:08:51 +0700

    Challenge #5

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ import { vertexShaderCode } from './vertexShaderCode.js';
 import { fragmentShaderCode } from './fragmentShaderCode.js';
 import { verticesEraser, verticesCube, verticesPlane } from './vertices.js';
 import { indicesEraser, indicesCube, indicesPlane } from './indices.js';
-import { mat4, mat3 } from "./gl-matrix/index.js";
+import { mat4, mat3, vec3 } from "./gl-matrix/index.js";
 import { mergeIndices, mergeVertices, getAllVerticesWithSurfaceNormal } from "./utils.js";
 
 window.onload = () => {
@@ -92,9 +92,9 @@ window.onload = () => {
 
     const uIsOn = gl.getUniformLocation(shaderProgram, "uIsOn");
 
-    const scale = 0.8;
+    const objectScale = 0.8;
     const uScale = gl.getUniformLocation(shaderProgram, "uScale");
-    gl.uniform1f(uScale, scale);
+    gl.uniform1f(uScale, objectScale);
 
     const camera = [0, 1, 3];
     const lightCube = [0, 0, 1];
@@ -108,30 +108,62 @@ window.onload = () => {
     mat4.translate(models[1], models[1], [0.7, 0, 0]);
     mat4.rotateY(models[1], models[1], -Math.PI / 2);
 
-    // model for cube;
-    mat4.translate(models[2], models[2], lightCube);
-
     // model for plane
     const bottomVertices = Math.min(...verticesEraser.map(elem => elem[1]));
-    mat4.translate(models[3], models[3], [0, bottomVertices*scale, 0]);
-
-    const view = mat4.create();
-    mat4.lookAt(view, camera, [camera[0], -0.1, 0], [0, 1, 0]);
-    gl.uniformMatrix4fv(uView, false, view);
-    gl.uniform3fv(uViewerPosition, camera);
-
-    gl.uniform3fv(uLightPosition, lightCube);
+    mat4.translate(models[3], models[3], [0, bottomVertices * objectScale, 0]);
 
     // constant for each model
     const shininessConstants = [5, 200, 0, 0];
     const ambientIntensities = [0.340, 0.340, 1, 1];
 
+    const lightCubeSpeed = 0.01;
+    let lightCubeMoveDirX = 0; // 0 - nothing, 1 - right, -1 - left
+    let lightCubeMoveDirZ = 0; // 0 - nothing, 1 - forward (away from screen), -1 - backward
+
+    const zoomSpeed = 0.01;
+    let zoomDir = 0; // 0 - nothing, 1 - zoom in, -1 - zoom out
+    const distanceMin = 1;
+
+    const cameraRotateSpeed = 0.01;
+    let cameraRotateDir = 0; // 0 - nothing, 1 - rotate right, -1 - rotate left
+    let rotation = 0;
+
     let isOn = true;
     window.addEventListener("keydown", (e) => {
-        if (e.code == "Space") {
+        if (e.code === "Space") {
             isOn = !isOn;
             ambientIntensities[2] = isOn;
         }
+
+        if (e.code === 'KeyD')
+            lightCubeMoveDirX = 1;
+        else if (e.code === 'KeyA')
+            lightCubeMoveDirX = -1;
+
+        if (e.code === 'KeyW')
+            lightCubeMoveDirZ = 1;
+        else if (e.code === 'KeyS')
+            lightCubeMoveDirZ = -1;
+
+        if (e.code === 'ArrowUp')
+            zoomDir = 1;
+        else if (e.code === 'ArrowDown')
+            zoomDir = -1;
+
+        if (e.code === 'ArrowRight')
+            cameraRotateDir = 1;
+        else if (e.code === 'ArrowLeft')
+            cameraRotateDir = -1;
+    });
+
+    window.addEventListener("keyup", (e) => {
+        if (e.code === 'KeyD' || e.code === 'KeyA') lightCubeMoveDirX = 0;
+
+        if (e.code === 'KeyW' || e.code === 'KeyS') lightCubeMoveDirZ = 0;
+
+        if (e.code === 'ArrowUp' || e.code === 'ArrowDown') zoomDir = 0;
+
+        if (e.code === 'ArrowRight' || e.code === 'ArrowLeft') cameraRotateDir = 0;
     });
 
 
@@ -139,6 +171,30 @@ window.onload = () => {
         gl.enable(gl.DEPTH_TEST);
         gl.clearColor(0.9, 0.9, 0.9, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        lightCube[0] += lightCubeMoveDirX * lightCubeSpeed;
+        lightCube[2] += -lightCubeMoveDirZ * lightCubeSpeed;
+
+        rotation += cameraRotateDir * cameraRotateSpeed;
+
+        const view = mat4.create();
+        const angle = vec3.angle(camera, [0,0,0]);
+        const curDistance = vec3.distance(camera, [0,0,0]);
+        const newDistance = curDistance - zoomDir * zoomSpeed;
+        if(newDistance >= distanceMin) {
+            camera[1] = Math.cos(angle) * newDistance;
+            camera[2] = Math.sin(angle) * newDistance;
+        }
+        const curCameraPosition = vec3.rotateY(vec3.create(), camera, [0, 0, 0], rotation);
+        mat4.lookAt(view, curCameraPosition, [0, 0, 0], [0, 1, 0]);
+
+        gl.uniformMatrix4fv(uView, false, view);
+        gl.uniform3fv(uViewerPosition, camera);
+
+        // model for cube;
+        mat4.translate(models[2], mat4.create(), lightCube);
+
+        gl.uniform3fv(uLightPosition, lightCube);
 
         gl.uniform1i(uIsOn, isOn);
 
